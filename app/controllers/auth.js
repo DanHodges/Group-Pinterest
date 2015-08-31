a48fc351ca8d550cd0f84e2bcb5b37cde56928dc
Taylor Miley <coachtaylormiley@gmail.com>
2015-08-31 15:18:04 -0500

    authentication functional

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,12 +1,15 @@
 app.controller("AuthCtrl", 
   ["$scope",
-   "$firebaseAuth"
-function($scope, $firebaseAuth) {
+  "$firebaseAuth",
+  function($scope, $firebaseAuth) {
     var ref = new Firebase("https://group-pinterest.firebaseio.com/pins");
-    ref.authWithOAuthRedirect("github", function(error) {
+    $scope.myValue = false;
+    ref.authWithOAuthPopup("github", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
+        $scope.myValue = true;
       } else {
-    // We'll never get here, as the page will redirect on success.
-    }
-});
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+}]);
