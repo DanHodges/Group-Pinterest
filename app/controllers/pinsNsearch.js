5560eb9594dacef9da07e032f0a8f1421bf5f5f3
Dan-Hodges <dhdgs690@gmail.com>
2015-08-31 13:05:57 -0500

    added files

--- app/controllers/pinsNsearch.js
+++ app/controllers/pinsNsearch.js
@@ -1,4 +1,6 @@
 app.controller("pinsNsearch", ["$scope", "getPics", function($scope, getPics){
+	var ref = new Firebase("https://group-pinterest.firebaseio.com/pins/");
+	$scope.pins = $firebaseArray(ref);
 
 	$scope.input = {
 		url: ''
@@ -6,17 +8,27 @@ app.controller("pinsNsearch", ["$scope", "getPics", function($scope, getPics){
 
 	$scope.photos = [];
 
+	$scope.pinIn = function() {
+		var pinItem = {
+			imgUrl: $scope.photo.img_o,
+			cat: '', 
+			caption: ""
+		};
+		$scope.songs.$add(pinItem);
+	};
+
 	$scope.click = function () {
 		var url = $scope.input.url;
 		getPics.getPics(url).then(
 			function(data) {
 				console.log("data :", data);
 				var photos = data.photos.photo;
-				photos = photos.filter(function(n){ return n != undefined });
+				photos = photos.filter(function(obj) {
+				    return (obj.url_o !== undefined);
+				});
 				for (var key in photos) {
 					$scope.photos.push(photos[key]);
 				}
-				console.log("$scope.photos :", $scope.photos);
 			}
 		)
 	}
