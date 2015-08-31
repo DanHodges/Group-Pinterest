631c68cd7e92e897fa4aae4c6b61e6208020b709
Dan-Hodges <dhdgs690@gmail.com>
2015-08-31 11:47:20 -0500

    added search feature

--- app/controllers/pinsNsearch.js
+++ app/controllers/pinsNsearch.js
@@ -4,11 +4,20 @@ app.controller("pinsNsearch", ["$scope", "getPics", function($scope, getPics){
 		url: ''
 	};
 
+	$scope.photos = [];
+
 	$scope.click = function () {
 		var url = $scope.input.url;
 		getPics.getPics(url).then(
-			function(data){
-				console.log("data", data);
-			})
+			function(data) {
+				console.log("data :", data);
+				var photos = data.photos.photo;
+				photos = photos.filter(function(n){ return n != undefined });
+				for (var key in photos) {
+					$scope.photos.push(photos[key]);
+				}
+				console.log("$scope.photos :", $scope.photos);
+			}
+		)
 	}
 }]);
